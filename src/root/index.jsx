b96600f9b30c5d7056344d0765337c9fee9852ef
AzizJakbaraliyev <azizjakbaraliyev454@gamil.com>
2025-02-09 18:57:05 +0500

    ProtectedRoute

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,59 +1,28 @@
-// import { createBrowserRouter } from "react-router-dom";
-// import Auth from "../components/auth";
-// import Verify from "../components/auth/verify";
-// import Home from "../pages/home";
-// import About from "../pages/about";
-// import Contact from "../pages/contact";
-
-// const root = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Auth />,
-//   },
-//   {
-//     path: "/verifly",
-//     element: <Verify />,
-//   },
-//   {
-//     path: "/home",
-//     element: <Home />,
-//   },
-//   {
-//     path: "/about",
-//     element: <About />,
-//   },
-//   {
-//     path: "/contact",
-//     element: <Contact />,
-//   },
-// ]);
-
-// export default root;
-
 import { createBrowserRouter } from "react-router-dom";
 import Auth from "../components/auth";
-import Verify from "../components/auth/verify";
 import Home from "../pages/home";
 import About from "../pages/about";
 import Contact from "../pages/contact";
+import ProtectedRoute from "../components/protectedRoute";
 import Layout from "../components/layouts";
-
+import Verify from "../components/auth/verify"; // To‘g‘ri ✅
 const root = createBrowserRouter([
+  { path: "/", element: <Auth /> },
+  { path: "/verifly", element: <Verify /> },
+
   {
     path: "/",
-    element: <Auth />,
-  },
-  {
-    path: "/verifly",
-    element: <Verify />,
-  },
-  {
-    path: "/",
-    element: <Layout />,
+    element: <ProtectedRoute />,
     children: [
-      { path: "home", element: <Home /> },
-      { path: "about", element: <About /> },
-      { path: "contact", element: <Contact /> },
+      {
+        path: "/",
+        element: <Layout />,
+        children: [
+          { path: "home", element: <Home /> },
+          { path: "about", element: <About /> },
+          { path: "contact", element: <Contact /> },
+        ],
+      },
     ],
   },
 ]);
